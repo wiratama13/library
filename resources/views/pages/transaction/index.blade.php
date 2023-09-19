c2651f45a3bcbac1b7a93dc8347e7ecb4d1e999e
wiratama13 <ashidiqinasrulloh@gmail.com>
2023-09-19 15:28:47 +0700

    push index

--- resources/views/pages/transaction/index.blade.php
+++ resources/views/pages/transaction/index.blade.php
@@ -121,15 +121,17 @@
             {data: 'member.name', name: 'member.name'},
             {data: 'days', name: 'days'},
             {data: 'amount', name: 'amount'},
-            {render: function (data){
-              let number = data;
+            {
+            //   render: function (data){
+            //   let number = data;
 
-              let sum = number.reduce(function (x, y) {
-                return x + y;
-              }, 0);
+            //   let sum = number.reduce(function (x, y) {
+            //     return x + y;
+            //   }, 0);
  
-              return sum.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")
-            }, data: 'details[].book.price', name: 'details[].book.price'},
+            //   return sum.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")
+            // }, 
+            data: 'details[].book.price', name: 'details[].book.price'},
             {data: 'status_tr', name: 'status_tr'},
             {
                 data: 'action', 
